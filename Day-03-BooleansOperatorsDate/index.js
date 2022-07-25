1c73350e2e1988915a997442f4a70653e272833b
learsito <ogeid_to@hotmail.com>
2022-07-24 23:24:55 -0500

    Day 3 started

---
+++ Day-03-BooleansOperatorsDate/index.js
@@ -0,0 +1,39 @@
+//Booleans
+    /*A boolean data type represents one of the two values: true or false. 
+    Boolean value is either true or false. The use of these data types will be  
+    clear when you start the comparison operator. Any comparisons return a 
+    boolean value which is either true or false. */
+
+    //Example: Boolean Values
+
+    let isLightOn = true
+    let isRaining = false
+    let isHungry = false 
+    let isMarried = true 
+    let trueValue = 4 > 3   // true
+    let falseValue = 4 < 3  //false
+
+    // We agreed that boolean values are either true or false.
+
+        // Truthy values
+            
+            /* All numbers (positive and negative) are truthy except zero
+                 All strings are truthy except an empty string ('')
+                 The boolean true */
+
+        // Falsy values 
+
+            /* 0
+                 On
+                 null
+                 undefuned
+                 NaN
+                 the boolean false
+                 '', "", ``, empty string */
+
+    /* It is good to remember those truthy values and falsy values.
+         In later section, we will use them with conditions to make decisions. */
+
+//Undefined
+
+    /* If we declare  */
