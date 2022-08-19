c61089ebb08617495acf5f9e493b362aaa6b6f2f
learsito <ogeid_to@hotmail.com>
2022-08-18 22:28:00 -0500

    Doing homework

--- Day-03-BooleansOperatorsDate/index.js
+++ Day-03-BooleansOperatorsDate/index.js
@@ -55,3 +55,60 @@
         let country = 'Mexico';
 
         //Assignment Operators 
+            /* Operator:        Example:            Same as:      Description:
+                    =                           x = y                       x = y               y is stored in x
+                    +=                      x += y                      x=x+y               x + y result is stored in x
+                    -=                       x-=y                       x=x-y               x - y result is stored in x                      
+                    *=                      x*=y                        x=x*y               x * y result is stored in x
+                    /=                      x/=y                        x=x/y               x / y result is stored in x                                                                    
+                    %=                      x%=y                        x=x%y               x % y result is stored in x                                                                            
+                    **=                     x**=y                      x=x**y            x ** y result is stored in x      */
+        //Arithmetic Operators
+        /* Arithmetic operators are mathematical operator.
+            1.- Addition(+): a+b
+            2.- Subtraction(-): a-b
+            3.- Multiplication(*): a*b
+            4.- Division(/): a/b
+            5.- Modulus(%): a %b
+            6.- Exponential(**): a ** b */
+
+            let numOne = 4;
+            let numTwo= 3;
+            let sum = numOne + numTwo;
+            let diff = numOne - numTwo;
+            let mult = numOne * numTwo;
+            let div = numOne / numTwo;
+            let remainder = numOne % numTwo;
+            let powerOf = numOne ** numTwo;
+
+            console.log(sum, diff, mult, div, remainder, powerOf); //  7, 1, 12, 1.33, 64
+
+            const PI = 3.14;
+            let radius = 100;           //length in meter
+
+            //Let us calculate area of a circle
+            const areaOfCircle = PI * radius * radius;
+            console.log(areaOfCircle); //       314 m
+
+            const gravity = 9.81        // in m/s2
+            let mass = 72                       // in Kilogram
+
+            // Let us calculate Weight of an object
+            const weight = mass * gravity;
+            console.log(weight);        //706.32 N(Newton)
+
+            const boilingPoint = 100 //temperature in oC, boiling point of water
+            const bodyTemp = 37            //body temperature in oC
+
+            //Concatenating string with numbers using string interpolation
+            /*
+                The boiling point of water is 100 oC.
+                HUman body temperature is 37 oC.
+                The gravity of earth is 9.81 m/s2.
+                */
+               console.log(
+                `The boiling point of water is ${boilingPoint} oC. \nHuman body temperature is ${bodyTemp} oC. \nThe gravity of earth is ${gravity} m / s2. `
+               );
+
+            //Comparison Operators
+                /*  In programming     */
